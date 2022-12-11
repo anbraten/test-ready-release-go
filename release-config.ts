684bd26c3162457b570bd5dd91dc4779b188597d
Anbraten <anton@ju60.de>
2022-12-11 08:48:40 +0100

    add to changelog

--- release-config.ts
+++ release-config.ts
@@ -32,6 +32,8 @@ export default defineConfig({
       `/app/changelog generate -m=${nextVersion} >> /app/CHANGELOG.md`
     );
 
+    await exec(`cat /app/CHANGELOG.md >> CHANGELOG.md`);
+
     await exec("git fetch origin main");
     await exec("git merge origin/main");
 
